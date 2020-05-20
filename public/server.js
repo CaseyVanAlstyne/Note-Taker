ca781f928e99b92ed05cd92043243dedcd46424a
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-05-20 08:50:09 -0400

    set up routes

--- public/server.js
+++ public/server.js
@@ -11,3 +11,16 @@ let PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Set up Routes for pathing
+// =============================================================
+// routes user to index html
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+// routes user to notes html
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+//static files (CSS)
+app.use(express.static(__dirname + '/public'));
