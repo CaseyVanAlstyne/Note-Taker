b19d1ce54f90459736e11a2ac415e866d226d749
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-05-20 10:19:00 -0400

    added start of newclass for js logic in reading and appending notes

--- public/assets/js/class.js
+++
@@ -1,5 +0,0 @@
-const util = require('util');
-const fs = require('fs');
-
-const readData = util.promisify(fs.readFile);
-const writeData = util.promisify(fs.writeFile);
---
+++ public/assets/js/newClass.js
@@ -0,0 +1,21 @@
+const util = require('util');
+const fs = require('fs');
+
+const readData = util.promisify(fs.readFile);
+const writeData = util.promisify(fs.writeFile);
+
+class newClass {
+    read() {
+        return readData("db/db.json", "utf8");
+    }
+    write(note) {
+        return writeData("db/db.json", JSON.stringify(note));
+    }
+    getNotes() {
+        return this.read()
+            .then(note => {
+                let pNote = JSON.parse(note);
+                return pNote;
+            })
+    }
+}
