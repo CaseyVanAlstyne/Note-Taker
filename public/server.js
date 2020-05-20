c34c811ada34f002dca249c57591a469a49d9a39
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-05-20 08:49:06 -0400

    set up express application

--- public/server.js
+++ public/server.js
@@ -2,3 +2,12 @@
 // =============================================================
 const express = require("express");
 const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+let PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
