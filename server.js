f9a0a52fe629b6625f84b685a439db48e528c75b
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-05-20 20:24:15 -0400

    working on getting note to display.

--- server.js
+++ server.js
@@ -33,17 +33,20 @@ app.get("/notes", function (req, res) {
 app.get("/api/notes", function (req, res) {
     return res.json(db);
     console.log(db)
-    newClass.getNote()
-        .then((note) => res.json(note))
-        // should read file and convert to array of objects, which is then registered to browser
-        .catch((err) => res.status(500).json(err));
-    // catches an error on status 500
+    // newClass.getNote()
+    //     .then((note) => res.json(note))
+    //     // should read file and convert to array of objects, which is then registered to browser
+    //     .catch((err) => res.status(500).json(err));
+    // // catches an error on status 500
 });
 
 //adds a new note
 app.post("/api/notes", function (req, res) {
     console.log(req.body);
     res.send("This was a success, dummy!");
+    res.json(note);
+    // res.append(data)
+    // return err
 });
 
 //deletes a note by id
